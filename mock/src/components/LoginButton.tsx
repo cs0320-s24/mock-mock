76f996f701bd4ec83f3edd8ef0adef2bd16526a8
astrid200408 <astridsanabria4@gmail.com>
2024-02-24 15:49:14 -0500

    pair-prog w/jgarci71, feat: added some minute styling

--- mock/src/components/LoginButton.tsx
+++ mock/src/components/LoginButton.tsx
@@ -1,26 +1,28 @@
-import { Dispatch, SetStateAction } from 'react';
-
+import { Dispatch, SetStateAction } from "react";
 
 interface loginProps {
-  isLoggedIn: boolean
-  setIsLoggedIn: Dispatch<SetStateAction<boolean>>
+  isLoggedIn: boolean;
+  setIsLoggedIn: Dispatch<SetStateAction<boolean>>;
 }
 
 export function LoginButton(props: loginProps) {
-
   const authenticate = () => {
-    const newValue = !props.isLoggedIn
-    props.setIsLoggedIn(newValue)
-    return newValue
-  }
+    const newValue = !props.isLoggedIn;
+    props.setIsLoggedIn(newValue);
+    return newValue;
+  };
 
   if (props.isLoggedIn) {
     return (
-      <button aria-label='Sign Out' onClick={authenticate}>Sign out</button>
-    )
+      <button className="login" aria-label="Sign Out" onClick={authenticate}>
+        Sign out
+      </button>
+    );
   } else {
     return (
-      <button aria-label='Login' onClick={authenticate}>Login</button>
-    )
+      <button className="login" aria-label="Login" onClick={authenticate}>
+        Login
+      </button>
+    );
   }
 }
