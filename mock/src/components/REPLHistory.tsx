e6779e16003ff0fe0bc19920cb05d587da9b3695
Juan <juan_garcia@brown.edu>
2024-02-24 13:11:39 -0500

    refactor: brought in the mock directory from gearup

---
+++ mock/src/components/REPLHistory.tsx
@@ -0,0 +1,16 @@
+import "../styles/main.css";
+
+interface REPLHistoryProps {
+  commands: string[];
+}
+export function REPLHistory(props: REPLHistoryProps) {
+  return (
+    <div className="repl-history" >
+      {props.commands.map((command, index) => (
+        <p aria-label={command}>{command}</p>
+      ))}
+      {/* This is where command history will go */}
+      {/* TODO: To go through all the pushed commands... try the .map() function! */}
+    </div>
+  );
+}
