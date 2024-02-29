3d9c24979eea9baea0b641337cd2a40c57479323
astrid200408 <astridsanabria4@gmail.com>
2024-02-29 09:52:10 -0500

    feat: working on e2e tests

--- mock/tests/e2e/Main.spec.ts
+++ mock/tests/e2e/Main.spec.ts
@@ -44,7 +44,7 @@ test("after I type into the input box, its text changes", async ({ page }) => {
   // Step 2: Interact with the page
   // Locate the element you are looking for
   await page.getByLabel("Command input").click();
-  await page.getByLabel("Command input").fill("Awesome command");``
+  await page.getByLabel("Command input").fill("Awesome command");
 
   // Step 3: Assert something about the page
   // Assertions are done by using the expect() function
